6b0deae1566c833a4d3a4445aee7d9e0859f61d0
Sami Klein <sami.klein@lessannoyingcrm.com>
2018-05-28 11:51:54 -0500

    5/28
    Tyler explained a better way to do a for loop. Committing because I want to look at what I had (for loop). About to build something better (foreach).

--- 17thblog/blogIndex.php
+++ 17thblog/blogIndex.php
@@ -6,32 +6,39 @@ include('config/init.php');
 echoNavAndHead("Blog Home");
 
 
-
-
-
-// echo "$blogPostsArray['0'][0]";
-// die("test2");
-
-// echo "<pre>";
-// var_dump($blogPostsArray);
-// echo "<pre>";
-
-
 // should be linking to some sort of variable,
 
 //lines 20/21 should def be changed
-echo "
 
+$iHopeThisWorks = getNumPosts();
+// echo $iHopeThisWorks["NumRows"];
+// echo "blah";
+// printNicely($iHopeThisWorks);
+// die("test");
+
+
+// sizeof() //pass in an array
+
+$posts = getAllBlogPosts();
+foreach($posts as $post){
+	echo "<h2>$post[title]</h2>";
+}
+die("test");
+
+echo "
 <body class='bg'>
 
 	<h1>My blog</h1>
 
 	<div class='textStyle'>
-		<p>WELCOME TO MY BLOG! IT'S GREAT! READ ABOUT MY LIFE</p>
-		<p><a href='blogPageFrame.php?postId=0'>Blog Post #1, 05/23/18</p>
-		<p><a href='blogPageFrame.php?postId=1'>Blog Post #2, 05/24/18</p>
-		<p><a href='blogPageFrame.php?postId=2'>Blog Post #3, 05/24/18</p>
-		<p><a href='blogPageFrame.php?postId=3'>Blog Post #4, 05/24/18</p>
+		<p>WELCOME TO MY BLOG! IT'S GREAT! READ ABOUT MY LIFE</p>";
+
+for ($i=0; $i<$iHopeThisWorks; $i++){
+	echo "
+	<p><a href='blogPageFrame.php?postId=$i'>Blog Post #$i, 05/23/18</p>
+	<br>";
+}
+echo "
 	</div>
 </body>
 ";
